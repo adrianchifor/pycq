58a597c619b27d8c762b3c056bd28998ab2a91fe
Adrian Chifor <me@adrianchifor.com>
2019-08-10 11:25:24 +0100

    Version increment 0.4.1

--- pycq/version.py
+++ pycq/version.py
@@ -1,7 +1,7 @@
 title = 'pycq'
 description = 'Simple distributed FIFO queue for Python using CockroachDB/PostgreSQL'
 url = 'https://github.com/adrianchifor/pycq'
-version = '0.4.0'
+version = '0.4.1'
 author = 'Adrian Chifor'
 author_email = 'me@adrianchifor.com'
 license = 'Apache License 2.0'
